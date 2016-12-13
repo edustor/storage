9ce22f6dd214a61be2eccb1f7bef38fbadf4b224
Dmitry Romanov <me@wutiarn.ru>
2016-12-13 14:01:42 +0300

    Apply target lesson from database

--- src/main/kotlin/ru/edustor/upload/service/PagesUploadService.kt
+++ src/main/kotlin/ru/edustor/upload/service/PagesUploadService.kt
@@ -7,18 +7,30 @@ import org.springframework.stereotype.Service
 import ru.edustor.commons.models.internal.processing.pdf.PdfUploadedEvent
 import ru.edustor.commons.models.upload.UploadResult
 import ru.edustor.commons.storage.service.BinaryObjectStorageService
+import ru.edustor.upload.repository.AccountRepository
 import java.io.InputStream
 import java.time.Instant
 import java.util.*
 
 @Service
-class PagesUploadService(val storage: BinaryObjectStorageService, val rabbitTemplate: RabbitTemplate) {
+class PagesUploadService(val storage: BinaryObjectStorageService,
+                         val rabbitTemplate: RabbitTemplate,
+                         val accountRepository: AccountRepository) {
     val logger: Logger = LoggerFactory.getLogger(PagesUploadService::class.java)
 
-    fun processFile(uploaderId: String, file: InputStream, fileSize: Long, targetLessonId: String? = null): UploadResult {
+    fun processFile(uploaderId: String, file: InputStream, fileSize: Long, requestedTarget: String? = null): UploadResult {
         val uploadUuid = UUID.randomUUID().toString()
 
         logger.info("Processing file $uploadUuid uploaded by $uploaderId")
+
+        val targetLessonId = requestedTarget ?: let {
+            val account = accountRepository.getOne(uploaderId)
+            val nuTarget = account?.nextUploadTarget ?: return@let null
+            account.nextUploadTarget = null
+            accountRepository.save(account)
+            logger.info("Using target lesson from database: $nuTarget")
+            return@let nuTarget
+        }
 
         storage.put(BinaryObjectStorageService.ObjectType.PDF_UPLOAD, uploadUuid, file, fileSize)
         logger.info("PDF $uploadUuid uploaded by $uploaderId")
