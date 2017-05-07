17e7144245c10c2eb50b87a959a6f69d3dfb36bc
Dmitry Romanov <me@wutiarn.ru>
2017-05-07 11:59:02 +0300

    Change EdustorStorageAccount table name

--- src/main/kotlin/ru/edustor/storage/model/EdustorStorageAccount.kt
+++ src/main/kotlin/ru/edustor/storage/model/EdustorStorageAccount.kt
@@ -2,8 +2,10 @@ package ru.edustor.storage.model
 
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.Table
 
 @Entity
+@Table(name = "accounts")
 class EdustorStorageAccount() {
     @Id lateinit var id: String
     var nextUploadTarget: String? = null
